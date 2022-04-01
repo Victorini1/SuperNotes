831475437052c6344fcd1807efd357ef4d8524b7
Baeonetta <victor.m.kennedy@gmail.com>
2022-04-01 00:39:25 -0500

    Notes are now displaying to notes.html, working on delete method

--- server.js
+++ server.js
@@ -2,27 +2,51 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 const path = require("path");
+const notes = require('./db.json'); // homework\SuperNotes\db.json
+const uuid = require('./uuid');
 
-app.listen(3001)
-
-
+console.log(notes)
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// GET API
-app.get('/api/notes'), (req, res) =>{
-    res.sendFile(path.join("./db.json"))
-}
+// GET API to read notes
+app.get('/api/notes', (req, res) =>{
+    console.log(notes);
+    res.json(notes)
+})
 
 
 //POST API to put notes into 
-app.post('/api/notes'), (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db.json'));
-    const newNotes = req.body;
-}
+app.post('/api/notes', (req, res) => {
+    const savedNote = JSON.parse(fs.readFileSync('./db.json'));
+    const note = req.body
+    // const newNotes = {
+    //     title: req.body.title,
+    //     text: req.body.text,
+    //     id: uuid()}
+        
+    savedNote.push(note);
+    fs.writeFileSync('./db.json', JSON.stringify(savedNote))
+    res.json(note);
+})
+// DELETE API
+
+app.delete('/api/notes/:id', (req, res) =>{
+      const note = req.body;
+      const id = req.params.id
+      notes.forEach((note) =>{
+          if (note.id === id){
+              notes.splice(id, 1)
+              return res.json(note)
+          }
+          if (err){
+              console.error(err)
+          }
+      })
+  })
 
 // Static HTML pages
 app.get('/', (req, res) => {
@@ -32,3 +56,6 @@ app.get('/', (req, res) => {
 app.get("/notes",  (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
+//Listening Port
+app.listen(3001)
